370addff47c94548332fc94c6e5e901368c4e305
angelmcff <97469886+angelmcff@users.noreply.github.com>
2022-12-20 20:09:21 +0100

    Update constantes.php

--- PHP_PROYECTO/constantes.php
+++ PHP_PROYECTO/constantes.php
@@ -1,8 +1,8 @@
 <?php
 
-define("HOST", "localhost");
-define("USERNAME", "root");
-define("PASSWORD", "");
+define("HOST", "database-1.cs0ubfehgyz2.us-east-1.rds.amazonaws.com");
+define("USERNAME", "admin");
+define("PASSWORD", "Monitor'2000");
 define("DATABASE", "libros2");
 
 ?>
